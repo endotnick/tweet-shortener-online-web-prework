c41935214e60f0d0fb1aa2e275770660dea00b49
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-26 01:17:42 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,20 @@
 # Write your code here.
-def word_substituter
+def dictionary
+  replacements = {
+    'hello' => 'hi',
+    'to' => 2,
+    'too' => 2,
+    'two' => 2,
+    'for' => 4,
+    'four' => 4,
+    'be' => 'b',
+    'you' => 'u',
+    'at' => '@',
+    'and' => '&'
+  }
+end
+
+def word_substituter(tweet)
 end
 
 def bulk_tweet
